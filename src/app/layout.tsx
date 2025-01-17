6c9a7d39c31d04d5c9a58396bdaed25ca9c2129a
ksj <naver.daum0711@gmail.com>
2025-01-17 21:26:45 +0900

    Refactor: meta data 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "GlobalNomad",
-  description: "GlobalNomad",
+  description: "당신의 일상을 특별하게 만드는 한 번의 클릭",
   icons: "/logo.svg",
 };
 
@@ -13,8 +14,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ko'>
-      <body>{children}</body>
-    </html>
+    <>
+      <Head>
+        <meta
+          name='description'
+          content='당신의 일상을 특별하게 만드는 한 번의 클릭'
+        />
+        <meta name='og:title' content='GlobalNomad' />
+        <meta
+          name='og:description'
+          content='당신의 일상을 특별하게 만드는 한 번의 클릭'
+        />
+        <meta name='og:image' content='/logo.svg' />
+      </Head>
+      <html lang='ko'>
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
