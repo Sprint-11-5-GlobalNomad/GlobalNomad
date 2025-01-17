3e69bdaac62d6b959c5df0d468eb2e5daa3a63ba
Stemplun <dnfl311@pusan.ac.kr>
2025-01-17 15:53:30 +0900

    feat: 크기에 따라 화면에 렌더링 안되는 현상 수정

--- src/types/button-type.ts
+++ src/types/button-type.ts
@@ -8,30 +8,37 @@ type ButtonSize = {
 // 버튼 크기 목록
 const ButtonSizes: Record<
   string,
-  { lg: ButtonSize; md?: ButtonSize; sm?: ButtonSize }
+  { lg?: ButtonSize; md?: ButtonSize; sm?: ButtonSize }
 > = {
   loginSignup: {
     // 로그인/회원가입
     lg: { width: 640, height: 48, radius: 6 },
     md: { width: 350, height: 48, radius: 6 },
+    sm: { width: 350, height: 48, radius: 6 },
   },
   modal: {
     // 모달
     lg: { width: 120, height: 48, radius: 8 },
-    md: { width: 138, height: 42, radius: 8 },
+    md: { width: 120, height: 48, radius: 8 },
+    sm: { width: 138, height: 42, radius: 8 },
   },
   modalDoubleButtons: {
     // 모달 2버튼
     lg: { width: 80, height: 38, radius: 6 },
+    md: { width: 80, height: 38, radius: 6 },
+    sm: { width: 80, height: 38, radius: 6 },
   },
   search: {
     // 검색하기
     lg: { width: 136, height: 56, radius: 4 },
+    md: { width: 136, height: 56, radius: 4 },
     sm: { width: 56, height: 26, radius: 4 },
   },
   availableTime: {
     // 예약 가능 시간
     lg: { width: 117, height: 46, radius: 8 },
+    md: { width: 117, height: 46, radius: 8 },
+    sm: { width: 117, height: 46, radius: 8 },
   },
   reservation: {
     // 예약하기
@@ -41,12 +48,14 @@ const ButtonSizes: Record<
   },
   dateReservation: {
     // 날짜 예약하기
-    lg: { width: 432, height: 56, radius: 4 }, // 테블릿
-    md: { width: 327, height: 56, radius: 4 }, // 모바일
+    md: { width: 432, height: 56, radius: 4 }, // 테블릿
+    sm: { width: 327, height: 56, radius: 4 }, // 모바일
   },
   profileSave: {
     // 프로필 저장하기 / 체험 등록하기 / 내 체험 수정
     lg: { width: 120, height: 48, radius: 4 },
+    md: { width: 120, height: 48, radius: 4 },
+    sm: { width: 120, height: 48, radius: 4 },
   },
   reviewCancel: {
     // 후기 작성 / 예약 취소
@@ -57,11 +66,14 @@ const ButtonSizes: Record<
   reviewSubmit: {
     // 후기 작성 제출
     lg: { width: 432, height: 56, radius: 4 },
-    md: { width: 350, height: 54, radius: 4 },
+    md: { width: 432, height: 56, radius: 4 },
+    sm: { width: 350, height: 54, radius: 4 },
   },
   approveReject: {
     // 승인하기 / 거절하기
     lg: { width: 82, height: 38, radius: 6 },
+    md: { width: 82, height: 38, radius: 6 },
+    sm: { width: 82, height: 38, radius: 6 },
   },
 };
 
