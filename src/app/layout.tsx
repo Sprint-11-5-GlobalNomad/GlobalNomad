1abe3ea49b7bc646d07afe036d50e8820e0540ee
ksj <naver.daum0711@gmail.com>
2025-01-17 16:14:28 +0900

    Refactor: favicon 재설정 및 메인 랜딩 페이지 목업 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 
-// import localFont from "next/font/local";
-// const pretendard = localFont({
-//   src: [
-//     {
-//       path: "/fonts/PretendardVariable.woff2",
-//       weight: "100 900",
-//       style: "normal",
-//     },
-//   ],
-//   variable: "--font-pretendard",
-// });
-
 export const metadata: Metadata = {
   title: "GlobalNomad",
   description: "GlobalNomad",
+  icons: "/logo.svg",
 };
 
 export default function RootLayout({
@@ -24,10 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <head>
-        <link rel='icon' href='/favicon.ico' />
-      </head>
+    <html lang='ko'>
       <body>{children}</body>
     </html>
   );
