2b4c63d0791b459a201eb5771d5f55a77d89b0dd
Stemplun <dnfl311@pusan.ac.kr>
2025-01-18 16:47:08 +0900

    feat: 모바일 크기 수정

--- src/components/my-activity-card.tsx
+++ src/components/my-activity-card.tsx
@@ -2,8 +2,8 @@
 
 import Image from "next/image";
 import { ActivityBasicDto } from "@/stores/types/activity-schemas";
-import Link from "next/link";
 import { useState } from "react";
+import Link from "next/link";
 
 type ActivityCardProps = Pick<
   ActivityBasicDto,
@@ -14,8 +14,9 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <div className="flex flex-col md:flex-row w-full lg:w-[80rem] lg:h-[20.4rem] md:w-[42.9rem] md:h-[15.6rem] sm:w-full rounded-[2.4rem] bg-white border border-gray-200 shadow-md lg:gap-[1rem] md:gap-0 p-[0.4rem]">
-      <div className="flex-shrink-0 w-full lg:w-[20.4rem] lg:h-[20.4rem] md:w-[15.6rem] md:h-[15.6rem] sm:w-full sm:h-full rounded-[2.4rem] overflow-hidden">
+    <div className="flex flex-col sm:flex-row w-[34.4rem] h-[12.8rem] sm:h-full md:flex-row lg:w-[80rem] lg:h-[20.4rem] md:w-[42.9rem] md:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 sm:gap-[1rem] p-[0.4rem]">
+      {/* 이미지 섹션 */}
+      <div className="flex-shrink-0 w-full h-[12.8rem] sm:w-[15.6rem] sm:h-[15.6rem] lg:w-[20.4rem] lg:h-[20.4rem] rounded-[2.4rem] overflow-hidden">
         <Image
           src={ActivityProps.bannerImageUrl}
           alt="체험 이미지"
@@ -25,49 +26,55 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
         />
       </div>
 
-      <div className="flex flex-col justify-between flex-1 p-2">
-        <div className="flex flex-col gap-4">
-          <div className="text-[1.4rem] lg:text-[1.6rem] md:text-[1.4rem] leading-[2.4rem] lg:leading-[2.6rem] font-pretendard-regular flex items-center gap-[0.2rem]">
+      {/* 내용 섹션 */}
+      <div className="flex flex-1 flex-col justify-between p-[1.2rem]">
+        {/* 레이팅 및 타이틀 */}
+        <div>
+          {/* 레이팅 */}
+          <div className="flex items-center gap-[0.4rem] text-[1.4rem] sm:text-[1.4rem] md:text-[1.4rem] lg:text-[1.6rem] leading-[2rem] sm:leading-[2.4rem] lg:leading-[2.6rem] font-pretendard-regular text-gray-800">
             <Image
               src="/image/rating-star.svg"
               alt="레이팅 이미지"
-              width={19}
-              height={19}
+              width={16}
+              height={16}
             />
-            <span>
-              {ActivityProps.rating} (
-              {ActivityProps.reviewCount.toLocaleString()})
-            </span>
+            <span className="font-bold">{ActivityProps.rating}</span>
+            <span className="text-gray-600">({ActivityProps.reviewCount})</span>
           </div>
-          <div className="text-[1.4rem] lg:text-[2rem] md:text-[1.8rem] leading-[2.4rem] lg:leading-[3.2rem] md:leading-[2.6rem] font-bold">
+
+          {/* 타이틀 */}
+          <div className="mt-[0.6rem] text-[1.6rem] sm:text-[1.6rem] md:text-[1.8rem] lg:text-[2rem] leading-[2.4rem] sm:leading-[2.4rem] lg:leading-[3.2rem] font-bold text-gray-900">
             {ActivityProps.title}
           </div>
         </div>
 
-        <div className="flex justify-between items-center">
-          <div className="text-[1.6rem] lg:text-[2rem] md:text-[2rem] leading-[2.6rem] lg:leading-[3.2rem] font-medium text-right">
-            ₩{ActivityProps.price.toLocaleString()}/인
+        {/* 가격 및 드롭다운 */}
+        <div className="flex justify-between items-center mt-[0.6rem]">
+          {/* 가격 */}
+          <div className="text-[1.6rem] sm:text-[1.6rem] md:text-[2rem] lg:text-[2rem] leading-[2.4rem] sm:leading-[2.4rem] lg:leading-[3.2rem] font-bold text-gray-900">
+            ₩{ActivityProps.price.toLocaleString()}
           </div>
 
+          {/* 드롭다운 */}
           <div className="relative">
             <button
-              className="w-[4rem] h-[4rem] flex justify-center items-center text-[1.6rem] font-bold"
+              className="w-[4rem] h-[4rem] flex justify-center items-center text-gray-500 hover:text-gray-800"
               onClick={() => setDropdownOpen(!dropdownOpen)}
             >
               ···
             </button>
             {dropdownOpen && (
-              <ul className="absolute right-0 mt-[0.2rem] w-[16rem] bg-white border border-gray-100 rounded-[0.6rem] shadow-lg">
-                <li className="h-[4.6rem] flex items-center justify-center cursor-pointer hover:bg-[var(--color-gray-100)] border-b border-gray-100">
+              <ul className="absolute right-0 mt-[0.2rem] w-[16rem] bg-white border border-gray-200 rounded-[0.6rem] shadow-lg">
+                <li className="h-[4.6rem] flex items-center justify-center cursor-pointer hover:bg-gray-100 border-b border-gray-200">
                   <Link
                     href="/edit"
-                    className="text-[1.8rem] font-medium leading-[2.6rem] font-pretendard"
+                    className="text-[1.6rem] font-medium leading-[2.4rem] text-gray-900"
                   >
                     수정하기
                   </Link>
                 </li>
-                <li className="h-[4.6rem] flex items-center justify-center cursor-pointer hover:bg-[var(--color-gray-100)]">
-                  <span className="text-[1.8rem] font-medium leading-[2.6rem] font-pretendard">
+                <li className="h-[4.6rem] flex items-center justify-center cursor-pointer hover:bg-gray-100">
+                  <span className="text-[1.6rem] font-medium leading-[2.4rem] text-red-500">
                     삭제하기
                   </span>
                 </li>
--- src/components/my-reservation-card.tsx
+++ src/components/my-reservation-card.tsx
@@ -14,8 +14,8 @@ type ReservationCardProps = Pick<
 
 export function MyReservationCard(ReservationProps: ReservationCardProps) {
   return (
-    <div className="flex flex-col md:flex-row w-full lg:w-[80rem] lg:h-[20.4rem] md:w-[42.9rem] md:h-[15.6rem] sm:w-full rounded-[2.4rem] bg-white border border-gray-200 shadow-md lg:gap-[1rem] md:gap-0 p-[0.4rem]">
-      <div className="flex-shrink-0 w-full lg:w-[20.4rem] lg:h-[20.4rem] md:w-[15.6rem] md:h-[15.6rem] sm:w-full sm:h-full rounded-[2.4rem] overflow-hidden">
+    <div className="flex flex-col sm:flex-row w-[34.4rem] h-[12.8rem] sm:h-full md:flex-row lg:w-[80rem] lg:h-[20.4rem] md:w-[42.9rem] md:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 sm:gap-[1rem] p-[0.4rem]">
+      <div className="flex-shrink-0 w-full h-[12.8rem] sm:w-[15.6rem] sm:h-[15.6rem] lg:w-[20.4rem] lg:h-[20.4rem] rounded-[2.4rem] overflow-hidden">
         <Image
           src={ReservationProps.activity.bannerImageUrl}
           alt={"체험 이미지"}
@@ -26,15 +26,33 @@ export function MyReservationCard(ReservationProps: ReservationCardProps) {
       </div>
       <div className="flex flex-col justify-between flex-1 p-2">
         <div className="flex flex-col gap-4">
-          <div className="text-[1.4rem] sm:text-[1.4rem] md:text-[1.6rem] lg:text-[1.6rem] leading-[2.6rem] font-bold">
-            {ReservationProps.status}
+          <div
+            className={`text-[1.4rem] sm:text-[1.4rem] md:text-[1.6rem] lg:text-[1.6rem] leading-[2.6rem] font-bold ${
+              ReservationProps.status === "pending"
+                ? "text-[var(--color-blue-light)]"
+                : ReservationProps.status === "confirmed"
+                  ? "text-[var(--color-orange)]"
+                  : ReservationProps.status === "declined"
+                    ? "text-[var(--color-red)]"
+                    : "text-[var(--color-gray-800)]"
+            }`}
+          >
+            {ReservationProps.status === "pending"
+              ? "예약 완료"
+              : ReservationProps.status === "confirmed"
+                ? "예약 승인"
+                : ReservationProps.status === "declined"
+                  ? "예약 거절"
+                  : ReservationProps.status === "canceled"
+                    ? "예약 취소"
+                    : "체험 완료"}
           </div>
           <div className="text-[1.4rem] lg:text-[2rem] md:text-[1.8rem] leading-[2.4rem] lg:leading-[3.2rem] md:leading-[2.6rem] font-bold">
             {ReservationProps.activity.title}
           </div>
           <div className="text-[1.2rem] sm:text-[1.2rem] md:text-[1.4rem] lg:text-[1.8rem] leading-[2.4rem] sm:leading-[2.4rem] md:leading-[2.4rem] lg:leading-[2.6rem] font-pretendard-regular">
-            {ReservationProps.date} · {ReservationProps.startTime} ~{" "}
-            {ReservationProps.endTime} · {ReservationProps.headCount}명
+            {ReservationProps.date}·{ReservationProps.startTime}~
+            {ReservationProps.endTime}·{ReservationProps.headCount}명
           </div>
         </div>
 
