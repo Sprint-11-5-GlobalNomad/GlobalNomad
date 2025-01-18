9ca6523f81079e74949f859be1ade7c98b6dc950
Stemplun <dnfl311@pusan.ac.kr>
2025-01-18 14:43:30 +0900

    feat: 반응형 추가

--- src/components/my-activity-card.tsx
+++ src/components/my-activity-card.tsx
@@ -14,8 +14,8 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <div className="flex w-[80rem] h-[20.4rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-[1rem] p-[0.4rem]">
-      <div className="flex-shrink-0 w-[20.4rem] h-[20.4rem] rounded-[2.4rem] overflow-hidden">
+    <div className="flex flex-col md:flex-row w-full lg:w-[80rem] lg:h-[20.4rem] md:w-[42.9rem] md:h-[15.6rem] sm:w-full rounded-[2.4rem] md:rounded-[2.4rem_0_0_0] bg-white border border-gray-200 shadow-md lg:gap-[1rem] md:gap-0 p-[0.4rem]">
+      <div className="flex-shrink-0 w-full lg:w-[20.4rem] lg:h-[20.4rem] md:w-[15.6rem] md:h-[15.6rem] sm:w-full sm:h-full rounded-[2.4rem] overflow-hidden">
         <Image
           src={ActivityProps.bannerImageUrl}
           alt="체험 이미지"
@@ -25,26 +25,26 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
         />
       </div>
 
-      <div className="flex flex-col justify-between flex-1">
+      <div className="flex flex-col justify-between flex-1 p-2">
         <div className="flex flex-col gap-4">
-          <div className="text-[1.6rem] leading-[2.6rem] font-pretendard-regular flex items-center gap-[0.2rem]">
+          <div className="text-[1.4rem] lg:text-[1.6rem] md:text-[1.4rem] leading-[2.4rem] lg:leading-[2.6rem] font-pretendard-regular flex items-center gap-[0.2rem]">
             <Image
               src="/image/rating-star.svg"
               alt="레이팅 이미지"
-              width={1.9 * 10}
-              height={1.9 * 10}
+              width={19}
+              height={19}
             />
             <span>
               {ActivityProps.rating} ({ActivityProps.reviewCount})
             </span>
           </div>
-          <div className="text-[2rem] leading-[3.2rem] font-pretendard-bold">
+          <div className="text-[1.4rem] lg:text-[2rem] md:text-[1.8rem] leading-[2.4rem] lg:leading-[3.2rem] md:leading-[2.6rem] font-bold">
             {ActivityProps.title}
           </div>
         </div>
 
         <div className="flex justify-between items-center">
-          <div className="text-[2rem] leading-[3.2rem] font-pretendard-bold">
+          <div className="text-[1.6rem] lg:text-[2rem] md:text-[2rem] leading-[2.6rem] lg:leading-[3.2rem] font-medium text-right">
             ₩{ActivityProps.price.toLocaleString()}/인
           </div>
 
