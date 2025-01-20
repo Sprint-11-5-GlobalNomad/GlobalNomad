07827e2fe3f6b919097134aaef71895f29ba8df0
ksj <naver.daum0711@gmail.com>
2025-01-20 21:16:14 +0900

    Refactor: 멘토님 조언 반영해 아이콘 svg 컴포넌트화 & 태그 수정

--- src/components/common/ui/category-dropdown.tsx
+++ src/components/common/ui/category-dropdown.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { DropdownCheckIcon } from "../icons/dropdown-check-icon";
+import { DropdownArrowIcon } from "../icons/dropdown-arrow-icon";
 
 interface CategoryDropdownProps {
   options: string[];
@@ -12,47 +14,17 @@ export default function CategoryDropdown({
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
-  const ArrowIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`w-6 h-6 transform transition-transform duration-500 ease-in-out ${isOpen ? "rotate-180" : "rotate-0"}`}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="black"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M7 9l5 5 5-5" />
-    </svg>
-  );
-
-  const CheckIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="w-5 h-5"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="20 6 9 17 4 12" />
-    </svg>
-  );
-
   return (
     <div
       className="relative w-full max-w-md"
       onClick={() => setIsOpen(!isOpen)}
     >
       <div className="relative w-[80rem] p-[1rem] border border-solid border-gray-900 rounded-[0.4rem] bg-white">
-        <p className="w-full text-lg bg-transparent text-gray-600 font-regular ml-[0.6rem]">
+        <span className="w-full text-lg bg-transparent text-gray-600 font-regular ml-[0.6rem]">
           {selectedOption || description}
-        </p>
+        </span>
         <div className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none">
-          <ArrowIcon />
+          <DropdownArrowIcon isOpen={isOpen} />
         </div>
       </div>
 
@@ -68,7 +40,7 @@ export default function CategoryDropdown({
               }}
             >
               <span className="opacity-0 group-hover:opacity-100 ml-[-10px] mr-4">
-                <CheckIcon />
+                <DropdownCheckIcon />
               </span>
               {option}
             </li>
