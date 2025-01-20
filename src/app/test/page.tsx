bf07a9981a27b087d6ffa304b86c2563636eb49e
ksj <naver.daum0711@gmail.com>
2025-01-20 17:32:47 +0900

    Refactor: dropdown CSS 잘못 설정된 것 수정 & 컴포넌트 간소화

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import DropDownMenu from "@/app/more-menu";
-import DropdownUse from "../../components/common/ui/card-drop-down";
+import FilterDropdown from "@/components/common/ui/filter-dropdown";
+import CategoryDropdown from "../../components/common/ui/category-dropdown";
+import EditDeleteDropdown from "@/components/common/ui/edit-delete-dropdown";
 
 export default function TestPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <div className="w-full max-w-4xl p-8 bg-white rounded-2xl shadow-lg space-y-8">
+      <div className="w-full max-w-4xl p-8 rounded-2xl shadow-lg space-y-8">
         <h1 className="text-2xl font-bold text-center">드롭다운 메뉴 테스트</h1>
 
-        <div className="grid grid-cols-2 gap-16">
+        <div className="flex-colmun gap-16">
           <div className="flex flex-col items-center space-y-4">
-            <h2 className="text-xl font-semibold">DropdownUse 컴포넌트</h2>
-            <DropdownUse />
+            <h2 className="text-xl font-semibold">카테고리 드롭다운</h2>
+            <CategoryDropdown
+              options={["문화 예술", "식음료", "스포츠", "투어", "관광"]}
+              description="카테고리"
+            />
           </div>
 
           <div className="flex flex-col items-center space-y-4 justify-start">
-            <h2 className="text-xl font-semibold">DropDownMenu 컴포넌트</h2>
+            <h2 className="text-xl font-semibold">수정 삭제 드롭다운</h2>
             <div className="flex w-full justify-end">
-              <DropDownMenu first="수정하기" second="삭제하기" />
+              <EditDeleteDropdown />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center space-y-4 justify-start">
+            <h2 className="text-xl font-semibold">필터 드롭다운</h2>
+            <div className="flex w-full justify-end">
+              <FilterDropdown />
             </div>
           </div>
         </div>
