76b09f21f8c04d65a76e2150884ad7536b97df5b
Stemplun <dnfl311@pusan.ac.kr>
2025-01-21 10:40:02 +0900

    feat: 별점/리뷰 시스템 구현

--- src/app/profile/reservation/component/review-modal.tsx
+++ src/app/profile/reservation/component/review-modal.tsx
@@ -1,16 +1,43 @@
+"use client";
+
 import Button from "@/components/button";
 import { ReservationResponseDto } from "@/stores/types/reservation-schemas";
+import { useState } from "react";
+import Image from "next/image";
 
 type ReviewModalProps = Pick<
   ReservationResponseDto,
   "activity" | "totalPrice" | "date" | "startTime" | "endTime" | "headCount"
->;
+> & { onClose: () => void };
+
+type ReviewState = {
+  rating: number;
+  content: string;
+};
+
 export function ReviewModal(Props: ReviewModalProps) {
+  const [review, setReview] = useState<ReviewState>({
+    rating: 0,
+    content: "",
+  });
+
+  function onChangeContent(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setReview((prev) => ({ ...prev, content: e.target.value }));
+  }
+
+  function onStarClick(index: number) {
+    setReview((prev) => ({ ...prev, rating: index + 1 }));
+  }
+
+  function onSubmitReview() {
+    // 제출 로직
+  }
+
   return (
-    <div>
+    <div className="">
       <div>
         <div>후기 작성</div>
-        <button>X</button>
+        <button onClick={Props.onClose}>X</button>
       </div>
       <div>
         <div>{Props.activity.bannerImageUrl}</div>
@@ -24,9 +51,28 @@ export function ReviewModal(Props: ReviewModalProps) {
         </div>
       </div>
       <div>
-        <div>별점</div>
-        <textarea placeholder="후기를 작성해주세요"></textarea>
-        <Button type="reviewSubmit" label="작성하기" />
+        <div className="flex gap-[0.5rem]">
+          {[...Array(5)].map((_, index) => (
+            <Image
+              key={index}
+              src={
+                index < review.rating
+                  ? "/image/rating-star.svg"
+                  : "/image/rating-empty-star.svg"
+              }
+              alt={index < review.rating ? "선택된 별점" : "빈 별점"}
+              width={24}
+              height={24}
+              onClick={() => onStarClick(index)}
+              style={{ cursor: "pointer" }}
+            />
+          ))}
+        </div>
+        <textarea
+          placeholder="후기를 작성해주세요"
+          onChange={onChangeContent}
+        ></textarea>
+        <Button type="reviewSubmit" label="작성하기" onClick={onSubmitReview} />
       </div>
     </div>
   );
