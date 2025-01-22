53ffcd45cff36099442118d1d49cd29f16b77a55
Stemplun <dnfl311@pusan.ac.kr>
2025-01-22 14:29:17 +0900

    feat: 체험 카드 추가 디자인 수정

--- src/components/my-activity-card.tsx
+++ src/components/my-activity-card.tsx
@@ -11,7 +11,7 @@ type ActivityCardProps = Pick<
 
 export function MyActivityCard(ActivityProps: ActivityCardProps) {
   return (
-    <div className="flex flex-row mobile:w-[34.4rem] mobile:h-[12.8rem] desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 mobile:gap-[1rem] p-[0.4rem]">
+    <div className="flex flex-row mobile:w-[34.4rem] mobile:h-[12.8rem] desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 p-[0.4rem]">
       <div className="flex-shrink-0 w-full h-[12.8rem] mobile:w-[12rem] mobile:h-[12rem] tablet:w-[14.8rem] tablet:h-[14.8rem] desktop:w-[19.6rem] desktop:h-[19.6rem] rounded-[2.4rem] overflow-hidden">
         <Image
           src={ActivityProps.bannerImageUrl}
@@ -21,7 +21,7 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="flex flex-1 flex-col justify-between p-[1.2rem]">
+      <div className="flex flex-1 flex-col justify-between p-[0.9rem]">
         <div>
           <div className="flex items-center gap-[0.4rem] text-[1.4rem] mobile:text-[1.4rem] tablet:text-[1.4rem] desktop:text-[1.6rem] leading-[2rem] mobile:leading-[2.4rem] desktop:leading-[2.6rem] font-pretendard-regular text-gray-800">
             <Image
@@ -33,7 +33,7 @@ export function MyActivityCard(ActivityProps: ActivityCardProps) {
             <span className="font-bold">{ActivityProps.rating}</span>
             <span className="text-gray-600">({ActivityProps.reviewCount})</span>
           </div>
-          <div className="mt-[0.6rem] text-[1.6rem] mobile:text-[1.6rem] tablet:text-[1.8rem] desktop:text-[2rem] leading-[2.4rem] mobile:leading-[2.4rem] desktop:leading-[3.2rem] font-bold text-gray-900">
+          <div className="mobile:text-[1.4rem] tablet:text-[1.8rem] desktop:text-[2rem] font-bold text-gray-900">
             {ActivityProps.title}
           </div>
         </div>
