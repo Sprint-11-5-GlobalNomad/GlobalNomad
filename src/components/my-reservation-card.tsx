942097e8dd529cc1a57f1bf6e946334e9e1ad76f
Stemplun <dnfl311@pusan.ac.kr>
2025-01-22 14:32:22 +0900

    feat: 예약 카드 디자인 수정

--- src/components/my-reservation-card.tsx
+++ src/components/my-reservation-card.tsx
@@ -42,36 +42,36 @@ export function MyReservationCard(ReservationProps: ReservationCardProps) {
   };
 
   return (
-    <div className="flex flex-col mobile:flex-row w-[34.4rem] h-[12.8rem] mobile:h-full tablet:flex-row desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 mobile:gap-[1rem] p-[0.4rem]">
-      <div className="flex-shrink-0 w-full h-[12.8rem] mobile:w-[15.6rem] mobile:h-[15.6rem] desktop:w-[20.4rem] desktop:h-[20.4rem] rounded-[2.4rem] overflow-hidden">
+    <div className="flex flex-row mobile:w-[34.4rem] mobile:h-[12.8rem] desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 p-[0.4rem]">
+      <div className="flex-shrink-0 w-full h-[12.8rem] mobile:w-[12rem] mobile:h-[12rem] tablet:w-[14.8rem] tablet:h-[14.8rem] desktop:w-[19.6rem] desktop:h-[19.6rem] rounded-[2.4rem] overflow-hidden">
         <Image
           src={ReservationProps.activity.bannerImageUrl}
           alt="체험 이미지"
-          width={204}
-          height={204}
+          width={200}
+          height={200}
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="flex flex-col justify-between flex-1 p-2">
-        <div className="flex flex-col gap-4">
+      <div className="flex flex-col justify-between flex-1 p-[0.9rem]">
+        <div className="flex flex-col gap-[0.4rem]">
           <div
-            className={`text-[1.4rem] mobile:text-[1.4rem] tablet:text-[1.6rem] desktop:text-[1.6rem] leading-[2.6rem] font-bold ${getStatusClasses(
+            className={`mobile:text-[1.4rem] tablet:text-[1.6rem] desktop:text-[1.6rem] leading-[2.6rem] font-bold ${getStatusClasses(
               ReservationProps.status
             )}`}
           >
             {getStatusText(ReservationProps.status)}
           </div>
-          <div className="text-[1.4rem] desktop:text-[2rem] tablet:text-[1.8rem] leading-[2.4rem] desktop:leading-[3.2rem] tablet:leading-[2.6rem] font-bold">
+          <div className="mobile:text-[1.4rem] desktop:text-[2rem] tablet:text-[1.8rem] leading-[2.4rem] desktop:leading-[3.2rem] tablet:leading-[2.6rem] font-bold">
             {ReservationProps.activity.title}
           </div>
-          <div className="text-[1.2rem] mobile:text-[1.2rem] tablet:text-[1.4rem] desktop:text-[1.8rem] leading-[2.4rem] mobile:leading-[2.4rem] tablet:leading-[2.4rem] desktop:leading-[2.6rem] font-pretendard-regular">
+          <div className="mobile:text-[1.2rem] tablet:text-[1.4rem] desktop:text-[1.8rem] leading-[2.4rem] mobile:leading-[2.4rem] tablet:leading-[2.4rem] desktop:leading-[2.6rem] font-pretendard-regular">
             {ReservationProps.date}·{ReservationProps.startTime}~
             {ReservationProps.endTime}·{ReservationProps.headCount}명
           </div>
         </div>
 
         <div className="flex justify-between items-center">
-          <div className="text-[1.6rem] desktop:text-[2rem] tablet:text-[2rem] leading-[2.6rem] desktop:leading-[3.2rem] font-medium text-right">
+          <div className="mobile:text-[1.6rem] desktop:text-[2rem] tablet:text-[2rem] leading-[2.6rem] desktop:leading-[3.2rem] font-medium text-right">
             ₩{ReservationProps.totalPrice.toLocaleString()}
           </div>
 
