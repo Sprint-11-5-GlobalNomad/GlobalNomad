65dd3e604fbe75b07c98cfa66a56ce50b44278e1
Stemplun <dnfl311@pusan.ac.kr>
2025-01-22 16:10:16 +0900

    feat: 충돌 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
-import Navbar from "@/components/common/layout/navbar";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../app/react-query/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Footer from "@/components/common/layout/footer";
 
 export const metadata: Metadata = {
   title: "GlobalNomad",
@@ -20,10 +19,9 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <QueryClientProvider client={queryClient}>
-          <Navbar />
-          {children}
-          {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
+          <main>{children}</main>
         </QueryClientProvider>
+        <Footer />
       </body>
     </html>
   );
