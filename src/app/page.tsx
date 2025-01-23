847903d63a6d05f506390c8e63edbb8851fdbf13
ksj <naver.daum0711@gmail.com>
2025-01-23 22:44:11 +0900

    Style: 메인 페이지 기본 ui 초안 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Button from "@/components/common/ui/button";
 import FilterDropdown from "@/components/common/ui/filter-dropdown";
-import SelectDropdown from "@/components/common/ui/select-dropdown";
 import Image from "next/image";
 import React from "react";
 
@@ -62,15 +61,16 @@ export default function Home() {
             />
           </button>
         </div>
-        <div>
+        <ul>
           {/* map 배열로 감싸주기 */}
+          {/* 이미지 크기 왜 제멋대로야 */}
           <Image
             src="/image/dance.svg"
             alt="스트릿 댄스 체험 이미지"
             width={384}
             height={384}
           />
-          <ul>
+          <div>
             <Image
               src="/image/rating-star.svg"
               alt="평균 별점 아이콘"
@@ -84,22 +84,52 @@ export default function Home() {
             <p>
               ₩ <span>/인</span>
             </p>
-          </ul>
-        </div>
+          </div>
+        </ul>
       </div>
 
       {/* 필터링 섹션 */}
       <div>
-        <div></div>
+        <div>{/* 버튼들 자리 */}</div>
+        <FilterDropdown
+          description="가격"
+          options={["가격이 낮은 순", "가격이 높은 순"]}
+          size="small"
+          // onSelect={}
+        />
       </div>
 
       {/* 모든 체험 목록 */}
       <div>
-        <div>
-          <h2>🛼 모든 체험</h2>
-        </div>
+        <h2>🛼 모든 체험</h2>
+        <ul>
+          {/* map 배열로 감싸주기 */}
+          <Image
+            src="/image/cliff.svg"
+            alt="피오르 체험 이미지"
+            width={283}
+            height={283}
+          />
+          {/* 여기 아래 부분 컴포넌트로 해서 쓸 수도 있겠다 */}
+          <div>
+            <Image
+              src="/image/rating-star.svg"
+              alt="평균 별점 아이콘"
+              width={18}
+              height={18}
+            />
+            <p>3.9 (108)</p>
+            {/* 251px 넘으면 overflow */}
+            {/* li 태그 이렇게 하면 괜찮으려나? */}
+            <li>피오르 체험</li>
+            <p>
+              ₩ <span>/인</span>
+            </p>
+          </div>
+        </ul>
       </div>
 
+      {/* 페이지네이션 */}
       <div className="text-[1.4rem]">1 2 3 4 5</div>
     </div>
   );
