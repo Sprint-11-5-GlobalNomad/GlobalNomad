67ee45e73c45ecf36823f67535fa94c1384698fc
Stemplun <dnfl311@pusan.ac.kr>
2025-01-23 14:50:15 +0900

    feat: 예약 취소 모달 연결

--- src/components/my-reservation-card.tsx
+++ src/components/my-reservation-card.tsx
@@ -1,3 +1,6 @@
+import { useCancelReservation } from "@/app/react-query/reservation-state";
+import ConfirmationModal from "@/components/confirmation-modal";
+import { useState } from "react";
 import Image from "next/image";
 import Button from "./common/ui/button";
 import { ReservationResponseDto } from "@/app/types/reservation-schemas";
@@ -11,7 +14,7 @@ type ReservationCardProps = Pick<
   | "endTime"
   | "headCount"
   | "totalPrice"
-> & { onCancelReservation: (reservationId: number) => void };
+>;
 
 export function MyReservationCard({
   activity,
@@ -21,8 +24,18 @@ export function MyReservationCard({
   endTime,
   headCount,
   totalPrice,
-  onCancelReservation,
 }: ReservationCardProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const cancelReservationMutation = useCancelReservation();
+
+  const handleCancelReservation = () => {
+    cancelReservationMutation.mutate({
+      reservationId: activity.id,
+      status: { status: "canceled" },
+    });
+    setIsModalOpen(false);
+  };
+
   const getStatusClasses = (status: string) => {
     switch (status) {
       case "pending":
@@ -54,49 +67,59 @@ export function MyReservationCard({
   };
 
   return (
-    <div className="flex flex-row mobile:w-[34.4rem] mobile:h-[12.8rem] desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 p-[0.4rem]">
-      <div className="flex-shrink-0 w-full h-[12.8rem] mobile:w-[12rem] mobile:h-[12rem] tablet:w-[14.8rem] tablet:h-[14.8rem] desktop:w-[19.6rem] desktop:h-[19.6rem] rounded-[2.4rem] overflow-hidden">
-        <Image
-          src={activity.bannerImageUrl}
-          alt="체험 이미지"
-          width={200}
-          height={200}
-          className="object-cover w-full h-full"
-        />
-      </div>
-
-      <div className="flex flex-col justify-between flex-1 desktop:py-[2.1rem] desktop:px-[2.4rem] tablet:py-[1.2rem] tablet:pl-[1.2rem] tablet:pr-[1.8rem] mobile:py-[1.1rem] mobile:pl-[0.8rem] mobile:pr-[1.5rem]">
-        <div className="flex flex-col desktop:gap-[0.8rem]">
-          <div
-            className={`font-bold text-[1.6rem] mobile:text-[1.4rem] ${getStatusClasses(status)}`}
-          >
-            {getStatusText(status)}
-          </div>
-          <div className="flex flex-col desktop:gap-[1.2rem] tablet:gap-[0.4rem]">
-            <div className="desktop:text-[2rem] tablet:text-[1.8rem] mobile:text-[1.4rem] font-bold">
-              {activity.title}
-            </div>
-            <div className="desktop:text-[1.8rem] tablet:text-[1.4rem] mobile:text-[1.2rem]">
-              {date}ㆍ{startTime}~{endTime}ㆍ{headCount}명
-            </div>
-          </div>
+    <>
+      <div className="flex flex-row mobile:w-[34.4rem] mobile:h-[12.8rem] desktop:w-[80rem] desktop:h-[20.4rem] tablet:w-[42.9rem] tablet:h-[15.6rem] rounded-[2.4rem] bg-white border border-gray-200 shadow-md gap-0 p-[0.4rem]">
+        <div className="flex-shrink-0 w-full h-[12.8rem] mobile:w-[12rem] mobile:h-[12rem] tablet:w-[14.8rem] tablet:h-[14.8rem] desktop:w-[19.6rem] desktop:h-[19.6rem] rounded-[2.4rem] overflow-hidden">
+          <Image
+            src={activity.bannerImageUrl}
+            alt="체험 이미지"
+            width={200}
+            height={200}
+            className="object-cover w-full h-full"
+          />
         </div>
 
-        <div className="flex justify-between items-center">
-          <div className="desktop:text-[2.4rem] tablet:text-[2rem] mobile:text-[1.6rem] font-medium text-right">
-            ₩{totalPrice.toLocaleString()}
+        <div className="flex flex-col justify-between flex-1 desktop:py-[2.1rem] desktop:px-[2.4rem] tablet:py-[1.2rem] tablet:pl-[1.2rem] tablet:pr-[1.8rem] mobile:py-[1.1rem] mobile:pl-[0.8rem] mobile:pr-[1.5rem]">
+          <div className="flex flex-col desktop:gap-[0.8rem]">
+            <div
+              className={`font-bold text-[1.6rem] mobile:text-[1.4rem] ${getStatusClasses(
+                status
+              )}`}
+            >
+              {getStatusText(status)}
+            </div>
+            <div className="flex flex-col desktop:gap-[1.2rem] tablet:gap-[0.4rem]">
+              <div className="desktop:text-[2rem] tablet:text-[1.8rem] mobile:text-[1.4rem] font-bold">
+                {activity.title}
+              </div>
+              <div className="desktop:text-[1.8rem] tablet:text-[1.4rem] mobile:text-[1.2rem]">
+                {date}ㆍ{startTime}~{endTime}ㆍ{headCount}명
+              </div>
+            </div>
           </div>
-          {status === "pending" && (
-            <Button
-              type="review"
-              label="예약 취소"
-              variant="outlined"
-              onClick={() => onCancelReservation(activity.id)}
-            />
-          )}
-          {status === "completed" && <Button type="review" label="후기 작성" />}
+
+          <div className="flex justify-between items-center">
+            <div className="desktop:text-[2.4rem] tablet:text-[2rem] mobile:text-[1.6rem] font-medium text-right">
+              ₩{totalPrice.toLocaleString()}
+            </div>
+            {status === "pending" && (
+              <Button
+                type="review"
+                label="예약 취소"
+                variant="outlined"
+                onClick={() => setIsModalOpen(true)} // 모달 열기
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <ConfirmationModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)} // 모달 닫기
+        onConfirm={handleCancelReservation} // 예약 취소
+        message="정말 예약을 취소하시겠습니까?"
+      />
+    </>
   );
 }
