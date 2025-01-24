c07b5731ff368a40315d2861882c00b06171197f
ksj <naver.daum0711@gmail.com>
2025-01-24 13:44:16 +0900

    Fix: 알림 텍스트 일부 색상 변경 오류 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: ["text-blue", "text-red", "text-green"],
   theme: {
     extend: {
       screens: {
