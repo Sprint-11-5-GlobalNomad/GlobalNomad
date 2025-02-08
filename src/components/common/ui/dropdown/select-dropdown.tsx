58388b7a090b5cc8974a85f8a07c19872adffc1d
Stemplun <dnfl311@pusan.ac.kr>
2025-02-08 14:25:57 +0900

    feat: 외부 클릭 감지 훅 사용

--- src/components/common/ui/dropdown/select-dropdown.tsx
+++ src/components/common/ui/dropdown/select-dropdown.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { DropdownArrowIcon } from "../../icons/dropdown-arrow-icon";
 import { DropdownCheckIcon } from "../../icons/dropdown-check-icon";
+import UseOutsideClick from "@/hooks/use-outside-click";
 
 interface SelectDropdownProps {
   options: string[];
   description: string;
-  value?: string; // 부모에서 받은 값
-  onChange?: (value: string) => void; // 선택된 값을 부모로 전달
+  value?: string;
+  onChange?: (value: string) => void;
   onBlur?: () => void;
 }
 
@@ -20,25 +21,11 @@ export default function SelectDropdown({
   onBlur,
 }: SelectDropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  // 외부 클릭 감지하여 드롭다운 닫기
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsOpen(false);
-        onBlur?.();
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const dropdownRef = UseOutsideClick(() => {
+    setIsOpen(false);
+    onBlur?.();
+  });
 
   return (
     <div className="relative w-full max-w-md" ref={dropdownRef}>
@@ -59,13 +46,13 @@ export default function SelectDropdown({
       </div>
 
       {isOpen && (
-        <ul className="absolute w-[79.2rem] tablet:w-[42.9rem] mobile:w-[34.3rem] mt-2 border border-gray-300 rounded-[0.6rem] bg-white z-50 p-[0.8rem] shadow-lg">
+        <ul className="absolute w-[79.2rem] tablet:w-[42.9rem] mobile:w-[34.3rem] mt-1 border border-gray-300 rounded-[0.6rem] bg-white z-50 p-[0.8rem] shadow-lg">
           {options.map((option) => (
             <li
               key={option}
               className="group px-8 py-6 text-lg rounded-[0.6rem] flex items-center hover:bg-green-dark hover:text-white cursor-pointer"
               onClick={() => {
-                onChange?.(option); // 부모로 값 전달
+                onChange?.(option);
                 setIsOpen(false);
                 onBlur?.();
               }}
