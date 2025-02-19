ff67f5098fb1a37b023f7cd2d4f08977bd402f6f
ksj <naver.daum0711@gmail.com>
2025-02-19 18:17:33 +0900

    Feat: 모바일 버전 체험 상세 이미지 이동 버튼 구현

--- src/components/pages/activity-detail/activity-images.tsx
+++ src/components/pages/activity-detail/activity-images.tsx
@@ -5,14 +5,41 @@ import Image from "next/image";
 import { useParams } from "next/navigation";
 import { LoadingIndicator } from "../../common/layout/indicator/loading-indicator";
 import { ErrorIndicator } from "../../common/layout/indicator/error-indicator";
+import { useEffect, useState } from "react";
 
 export default function ActivityImages() {
   const { id } = useParams();
   const { data: activity, isLoading, isError } = useActivityDetail(Number(id));
-  const subImageCount = activity?.subImages.length;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 743);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  const totalImages = activity?.subImages
+    ? activity.subImages.length + 1
+    : activity?.bannerImageUrl
+      ? 1
+      : 0;
 
   if (isLoading) return <LoadingIndicator width={100} height={100} />;
   if (isError) return <ErrorIndicator width={100} height={100} />;
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
+  };
 
   return (
     <div
@@ -20,52 +47,107 @@ export default function ActivityImages() {
     tablet:w-[72rem] tablet:h-[31rem] tablet:mb-[3.2rem]
     mobile:w-full mobile:h-[31rem] mobile:mb-[1.5rem]"
     >
-      {subImageCount === 0 ? (
-        <Image
-          src={activity!.bannerImageUrl}
-          alt={`${activity?.title} 배너 이미지`}
-          width={1198}
-          height={534}
-          className="rounded-[1.2rem] w-full h-full object-cover"
-        />
-      ) : (
-        <div
-          className="w-[119.8rem] h-[53.4rem] flex gap-[0.8rem] mb-[8.5rem]
-        tablet:w-[72rem] tablet:h-[31rem] tablet:mb-[3.2rem] mobile:w-full mobile:h-[31rem]"
-        >
-          <Image
-            src={activity!.bannerImageUrl}
-            alt={`${activity?.title} 배너 이미지`}
-            width={595}
-            height={534}
-            className="rounded-[1.2rem] tablet:w-[40vw] tablet:h-[31rem]"
-          />
-          <div
-            className={`grid gap-[0.8rem] ${
-              subImageCount === 1
-                ? "grid-cols-1 grid-rows-1"
-                : subImageCount === 2
-                  ? "grid-cols-1 grid-rows-2"
-                  : "grid-cols-2 grid-rows-2"
-            } w-full h-full`}
+      {isMobile ? (
+        <>
+          <button
+            onClick={prevImage}
+            className={`w-[2.4rem] h-[4.7rem]
+    absolute top-1/2 translate-y-[-50%] left-[1.6rem] ${totalImages === 1 ? "hidden" : ""}`}
+            // disabled={activity?.subImages.length === 0}
           >
-            {activity?.subImages.map((subImage, index) => {
-              const isThirdImageWithFullWidth =
-                subImageCount === 3 && index === 2;
+            <Image
+              src="/image/btn_pagination_arrow_left.svg"
+              alt="이전 이미지 보기"
+              width={24}
+              height={47}
+            />
+          </button>
+          {currentIndex === 0 ? (
+            <div className="mobile:w-full mobile:h-[31rem]">
+              <Image
+                src={activity!.bannerImageUrl}
+                alt={`${activity?.title} 이미지 ${currentIndex + 1}`}
+                width={595}
+                height={310}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ) : (
+            <div className="mobile:w-full mobile:h-[31rem]">
+              <Image
+                src={activity?.subImages?.[currentIndex - 1]?.imageUrl || ""}
+                alt={`${activity?.title} 이미지 ${currentIndex + 1}`}
+                width={595}
+                height={310}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          )}
+          <button
+            onClick={nextImage}
+            className={`w-[2.4rem] h-[4.7rem]
+          absolute top-1/2 translate-y-[-50%] right-[1.6rem] ${totalImages === 1 ? "hidden" : ""}`}
+            disabled={activity?.subImages.length === 0}
+          >
+            <Image
+              src="/image/btn_pagination_arrow_right.svg"
+              alt="다음 이미지 보기"
+              width={24}
+              height={47}
+            />
+          </button>
+        </>
+      ) : (
+        <div>
+          {totalImages > 1 ? (
+            <Image
+              src={activity!.bannerImageUrl}
+              alt={`${activity?.title} 배너 이미지`}
+              width={1198}
+              height={534}
+              className="rounded-[1.2rem] w-full h-full object-cover"
+            />
+          ) : (
+            <div
+              className="w-[119.8rem] h-[53.4rem] flex gap-[0.8rem] mb-[8.5rem]
+        tablet:w-[72rem] tablet:h-[31rem] tablet:mb-[3.2rem]"
+            >
+              <Image
+                src={activity!.bannerImageUrl}
+                alt={`${activity?.title} 이미지 ${currentIndex + 1}`}
+                width={595}
+                height={534}
+                className="rounded-[1.2rem] tablet:w-[40vw] tablet:h-[31rem]"
+              />
 
-              return (
-                <Image
-                  key={subImage.id}
-                  src={subImage.imageUrl}
-                  alt={`${activity.title} 소개 이미지 ${index + 1}`}
-                  width={isThirdImageWithFullWidth ? 595 : 293.5}
-                  height={263}
-                  className={`${isThirdImageWithFullWidth ? "col-span-2 w-full" : "w-full h-full"}
+              <div
+                className={`grid gap-[0.8rem] ${
+                  totalImages === 2
+                    ? "grid-cols-1 grid-rows-1"
+                    : totalImages === 3
+                      ? "grid-cols-1 grid-rows-2"
+                      : "grid-cols-2 grid-rows-2"
+                } w-full h-full`}
+              >
+                {activity?.subImages.map((subImage, index) => {
+                  const isThirdImageWithFullWidth =
+                    totalImages === 4 && index === 2;
+
+                  return (
+                    <Image
+                      key={subImage.id}
+                      src={subImage.imageUrl}
+                      alt={`${activity.title} 이미지 ${currentIndex + 1}`}
+                      width={isThirdImageWithFullWidth ? 595 : 293.5}
+                      height={263}
+                      className={`${isThirdImageWithFullWidth ? "col-span-2 w-full" : "w-full h-full"}
                   w-full h-full object-cover rounded-[1.2rem]`}
-                />
-              );
-            })}
-          </div>
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
