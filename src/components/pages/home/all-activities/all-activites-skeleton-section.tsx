2aca7154b24ad62b74226f16bc4cd888a4bb7045
Stemplun <dnfl311@pusan.ac.kr>
2025-02-22 10:04:20 +0900

    feat: 하이드레이션 에러 해결 및 디바이스 별 갯수 맞도록 수정

--- src/components/pages/home/all-activities/all-activites-skeleton-section.tsx
+++ src/components/pages/home/all-activities/all-activites-skeleton-section.tsx
@@ -1,29 +1,4 @@
-import { useEffect, useState } from "react";
-
 export default function AllActivitiesSkeletonSection() {
-  const [windowWidth, setWindowWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 1024 // 초기값을 적당한 값으로 설정
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const displayedActivities =
-    windowWidth >= 1024
-      ? [0, 1, 2, 3, 4, 5, 6, 7]
-      : windowWidth < 744
-        ? [0, 1, 2, 3]
-        : [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
   return (
     <div
       className="grid grid-cols-4 gap-[2rem] w-[120rem] mb-[6rem]
@@ -31,13 +6,42 @@ export default function AllActivitiesSkeletonSection() {
     mobile:grid-cols-2 mobile:grid-rows-2 mobile:grid-auto-rows-[1fr]
     mobile:w-[38.8rem] mobile:mb-[4.6rem] mobile:px-[2rem] mobile:gap-[1.6rem]"
     >
-      {displayedActivities.map((_, index) => (
-        <div
-          key={index}
-          className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
             tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton"
-        />
-      ))}
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+          tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+        tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+      tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+    tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton mobile:hidden"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+    tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton mobile:hidden"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+    tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton mobile:hidden"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+    tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton mobile:hidden"
+      />
+      <div
+        className="h-[41.4rem] w-[28.3rem] tablet:w-[22.1rem] rounded-[2rem]
+    tablet:h-[34.7rem] mobile:w-[16.8rem] mobile:h-[28.3rem] skeleton mobile:hidden desktop:hidden"
+      />
     </div>
   );
 }
