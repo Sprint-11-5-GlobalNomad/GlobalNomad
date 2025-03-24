18472aba4f9ae536fb98574dd549ba50dd70ec6c
ksj <naver.daum0711@gmail.com>
2025-03-24 14:57:32 +0900

    Refactor: 스크롤 후 현재 위치 계산하는 로직에 디바운싱 추가

--- src/app/(primary)/layout.tsx
+++ src/app/(primary)/layout.tsx
@@ -8,8 +8,9 @@ import Script from "next/script";
 const KAKAO_MAP = process.env.NEXT_PUBLIC_KAKAO_MAP_APP_KEY;
 
 export const metadata: Metadata = {
-  title: "GlobalNomad",
-  description: "당신의 일상을 특별하게 만드는 한 번의 클릭",
+  title: "특별한 이색 체험, 원데이 클래스를 찾을 수 있는 곳 | GlobalNomad",
+  description:
+    "당신의 일상을 특별하게 만드는 한 번의 클릭. 원데이 클래스, 여행 가이드, 동아리, 체험 입장권 등 다양한 체험 상품들을 예약할 수 있습니다.",
   icons: "/logo.svg",
 };
 
